7796595bd32b4a8f944f1a358b2567dbdc7c5937
Aspian <muhammad.aspian.d@gmail.com>
2026-09-05 09:35:49 +0800

    add nix dev shell

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+  description = "";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          devShells.default = pkgs.mkShell {
+            packages = with pkgs; [
+              bun
+              rustic
+              php84
+              php84Packages.composer
+            ];
+          };
+        };
+    };
+}
